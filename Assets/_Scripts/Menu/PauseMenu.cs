6102cde29986a234dd5945aeb28aa5d11565d0c3
undefined <paul.gaston@me.com>
2018-04-19 16:41:35 +0200

    Pause menu

---
+++ Assets/_Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool gameIsPaused;
+    private GameObject pauseMenuUI;
+
+	private void Start()
+	{
+        pauseMenuUI = transform.GetChild(0).gameObject;
+	}
+
+	private void Update()
+	{
+        if(Input.GetButtonDown("Pause"))
+        {
+            if(gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+	public void Pause()
+	{
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+	}
+
+    public void SaveAndQuit()
+    {
+        Resume();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
