f5a6e50e4ff7122cfde9ec152cbd35a06962ab58
undefined <paul.gaston@me.com>
2018-05-21 17:41:31 +0200

    Clock

---
+++ Assets/ClockManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClockManager : MonoBehaviour {
+    TimeManager time;
+    TextMeshProUGUI textMesh; 
+	// Use this for initialization
+	void Start () 
+    {
+        time = TimeManager.Instance;
+        textMesh = GetComponentInChildren<TextMeshProUGUI>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        int demie = Mathf.FloorToInt(time.clock.minute / 30) * 30;
+        string hour = time.clock.hour + " : " + demie;
+        textMesh.text = hour;
+	}
+}
