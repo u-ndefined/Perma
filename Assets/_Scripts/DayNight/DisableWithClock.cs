1f7d91442bd3abd3ee1c9c84ca1adc43371338a9
undefined <paul.gaston@me.com>
2018-05-25 15:17:34 +0200

    Desactive with clock

---
+++ Assets/_Scripts/DayNight/DisableWithClock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableWithClock : MonoBehaviour 
+{
+    public Clock start, end;
+    public bool disable;
+    private bool isActive = true;
+
+
+	private void Start()
+	{
+        if (TimeManager.Instance.clock > start && TimeManager.Instance.clock < end) Toggle(!disable);
+        else Toggle(disable);
+	}
+
+	// Update is called once per frame
+	void Update () 
+    {
+        if(disable == isActive && TimeManager.Instance.clock > start && TimeManager.Instance.clock < end)
+        {
+            Toggle(!disable);
+        }
+        if(disable != isActive)
+        {
+            if (TimeManager.Instance.clock < start || TimeManager.Instance.clock > end)
+            {
+                Toggle(disable);
+            }
+        }
+	}
+
+    public void Toggle(bool active)
+    {
+        isActive = active;
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(active);
+        }
+    }
+}
